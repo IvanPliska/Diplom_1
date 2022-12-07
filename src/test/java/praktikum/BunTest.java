ee0d2db349fd08bb7dc0e60f7492f2fa01014baa
IvanPliska <94367970+IvanPliska@users.noreply.github.com>
2022-12-07 15:34:53 +0300

    Change BunTest

--- src/test/java/praktikum/BunTest.java
+++ src/test/java/praktikum/BunTest.java
@@ -5,9 +5,10 @@ import static org.junit.Assert.*;
 
 public class BunTest {
 
+    Bun bun = new Bun("black bun", 100);
+
     @Test
     public void shouldGetName() {
-        Bun bun = new Bun("black bun", 100);
         String expected = "black bun";
         String actual = bun.getName();
         assertEquals("Name bun is incorrect", expected, actual);
@@ -15,7 +16,6 @@ public class BunTest {
 
     @Test
     public void getPrice() {
-        Bun bun = new Bun("black bun", 100);
         float expected = 100;
         float actual = bun.getPrice();
         assertEquals("Price bun is incorrect", expected, actual, 0);
