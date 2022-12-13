9ff86d0b487b77e8f3b503f70b9426fc555d7eb1
IvanPliska <94367970+IvanPliska@users.noreply.github.com>
2022-12-13 11:12:28 +0300

    Changes BunTest.class, IngredientType.class, pom.xml

--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -4,14 +4,17 @@ package praktikum;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class IngredientTest {
 
-    IngredientType ingredientType = IngredientType.SAUCE;
+    IngredientType ingredientType_1 = IngredientType.SAUCE;
+    IngredientType ingredientType_2 = IngredientType.FILLING;
+    IngredientType ingredientType_3 = null;
 
     @Test
     public void shouldGetName() {
-        Ingredient ingredient = new Ingredient(ingredientType, "white bun", 200);
+        Ingredient ingredient = new Ingredient(ingredientType_1, "white bun", 200);
         String expectedName = "white bun";
         String actualName = ingredient.getName();
         assertEquals("Name ingredient is incorrect", expectedName, actualName);
@@ -20,7 +23,7 @@ public class IngredientTest {
 
     @Test
     public void shouldGetPrice() {
-        Ingredient ingredient = new Ingredient(ingredientType, "red bun", 300);
+        Ingredient ingredient = new Ingredient(ingredientType_1, "red bun", 300);
         float expectedPrice = 300;
         float actualPrice = ingredient.getPrice();
         assertEquals("Price ingredient is incorrect", expectedPrice, actualPrice, 0);
@@ -28,11 +31,26 @@ public class IngredientTest {
     }
 
     @Test
-    public void shouldGetType() {
-        Ingredient ingredient = new Ingredient(ingredientType, "white bun", 200);
+    public void shouldGetTypeFirstIngredientType() {
+        Ingredient ingredient = new Ingredient(ingredientType_1, "white bun", 200);
         IngredientType expectedType = IngredientType.SAUCE;
         IngredientType actualType = ingredient.getType();
         assertEquals("Type ingredient is incorrect", expectedType, actualType);
+    }
+
+    @Test
+    public void shouldGetTypeSecondIngredientType() {
+        Ingredient ingredient = new Ingredient(ingredientType_2, "white bun", 200);
+        IngredientType expectedType = IngredientType.FILLING;
+        IngredientType actualType = ingredient.getType();
+        assertEquals("Type ingredient is incorrect", expectedType, actualType);
+    }
+
+    @Test
+    public void shouldGetTypeThirdIngredientType() {
+        Ingredient ingredient = new Ingredient(ingredientType_3, "white bun", 200);
+        IngredientType actualType = ingredient.getType();
+        assertNull("Type ingredient is incorrect", actualType);
     }
 
 }
